1873dda8d77b4e16ff7694b9dc50acf786f0b0b6
FelixvL <digitaleservice@gmail.com>
2023-01-10 14:54:39 +0100

    app.py aangepaste

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import felix
+import omar
+import jos
 
 app = Flask(__name__)
 
@@ -10,3 +12,12 @@ def hello_world():
 @app.route("/felix")
 def ffelix():
     return felix.vanfelix()
+
+
+@app.route("/omar")
+def fomar():
+    return omar.vanomar()
+
+@app.route("/jos")
+def fjos():
+    return jos.vanjos()
